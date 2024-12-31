2368e2a87c6ccecc19805aa6dc4ee662dc14a501
The Radius <aqibhussain2007@gmail.com>
2024-12-31 08:29:53 +0500

    light fix

--- main/main.js
+++ main/main.js
@@ -5,16 +5,3 @@ function switchToLogin() {
 function switchToRegister() {
     window.location.href = "main/register.html";
 }
-
-function validateForm(event) {
-    event.preventDefault();
-    const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('emailError');
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailPattern.test(emailInput.value)) {
-        emailError.textContent = 'Please enter a valid email.';
-    } else {
-        emailError.textContent = 'Form submitted successfully!';
-    }
-}
